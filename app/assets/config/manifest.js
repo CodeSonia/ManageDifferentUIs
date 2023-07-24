a6bfd444431a083c4c8edfecb66743442e063be7
Sonia Hussain <sonia_ahm@icloud.com>
2023-07-24 09:45:27 +0100

    Rendered different CSS files

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -2,3 +2,5 @@
 //= link_directory ../stylesheets .css
 //= link_tree ../../javascript .js
 //= link_tree ../../../vendor/javascript .js
+//= link version_1/version_1.css
+//= link version_2/version_2.css
