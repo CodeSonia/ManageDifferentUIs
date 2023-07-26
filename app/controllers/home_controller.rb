f9464a3ac473555d61c33103e05a5d4a6c4715cb
Sonia Hussain <sonia_ahm@icloud.com>
2023-07-26 16:41:13 +0100

    Able to render version_one page_one

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -16,13 +16,23 @@ class HomeController < ApplicationController
 
   # Set up 2 methods to handle 2 different views
   # Note this is not the standard rails version:
+  # def version_one
+  #   @css_file = 'version_one/version_one.css'
+  #   render 'home/version_one/version_one', layout: 'layouts/version_one/version_one'
+  # end
+
+  # def version_two
+  #   @css_file = 'version_two/version_two.css'
+  #   render 'home/version_two/version_two', layout: 'layouts/version_two/version_two'
+  # end
+
   def version_one
     @css_file = 'version_one/version_one.css'
     render 'home/version_one/version_one', layout: 'layouts/version_one/version_one'
   end
 
-  def version_two
-    @css_file = 'version_two/version_two.css'
-    render 'home/version_two/version_two', layout: 'layouts/version_two/version_two'
+  def version_one_page_one
+    @css_file = 'version_one/version_one.css'
+    render 'home/version_one/page_one', layout: 'layouts/version_one/version_one'
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,11 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
   # get '/homepage/:design_version', to: 'home#homepage', as: 'homepage'
+  # get '/version-one', to: 'home#version_one', as: 'version-one'
+  # get '/version-two', to: 'home#version_two', as: 'version-two'
+
   get '/version-one', to: 'home#version_one', as: 'version-one'
-  get '/version-two', to: 'home#version_two', as: 'version-two'
+  get '/version-one/page-one', to: 'home#version_one_page_one', as: 'version-one-page-one'
+  get '/version-one/page-two', to: 'home#version_one_page_two', as: 'version-one-page-two'
+  get '/version-one/page-three', to: 'home#version_one_page_three', as: 'version-one-page-three'
 end
