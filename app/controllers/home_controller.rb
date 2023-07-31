f122fa7ed51743bb6fdaf9ed7cad5a341bc22a8e
Sonia Hussain <sonia_ahm@icloud.com>
2023-07-31 09:39:40 +0100

    Refactored home controller

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,68 +1,58 @@
 # frozen_string_literal: true
 
 class HomeController < ApplicationController
-  # def homepage
-  #   design_version = params[:design_version]
-
-  #   case design_version
-  #   when 'version_one'
-  #     @css_file = 'version_one/version_one.css'
-  #   when 'version_two'
-  #     @css_file = 'version_two/version_two.css'
-  #   end
-
-  #   render 'homepage'
-  # end
-
-  # Set up 2 methods to handle 2 different views
-  # Note this is not the standard rails version:
-  # def version_one
-  #   @css_file = 'version_one/version_one.css'
-  #   render 'home/version_one/version_one', layout: 'layouts/version_one/version_one'
-  # end
-
-  # def version_two
-  #   @css_file = 'version_two/version_two.css'
-  #   render 'home/version_two/version_two', layout: 'layouts/version_two/version_two'
-  # end
+  before_action :set_css_file, only: [
+    :version_one,
+    :version_one_page_one,
+    :version_one_page_two,
+    :version_one_page_three,
+    :version_two,
+    :version_two_page_one,
+    :version_two_page_two,
+    :version_two_page_three
+  ]
 
   def version_one
-    @css_file = 'version_one/version_one.css'
     render 'home/version_one/version_one', layout: 'layouts/version_one/version_one'
   end
 
   def version_one_page_one
-    @css_file = 'version_one/version_one.css'
     render 'home/version_one/page_one', layout: 'layouts/version_one/version_one'
   end
 
   def version_one_page_two
-    @css_file = 'version_one/version_one.css'
     render 'home/version_one/page_two', layout: 'layouts/version_one/version_one'
   end
 
   def version_one_page_three
-    @css_file = 'version_one/version_one.css'
     render 'home/version_one/page_three', layout: 'layouts/version_one/version_one'
   end
 
   def version_two
-    @css_file = 'version_two/version_two.css'
     render 'home/version_two/version_two', layout: 'layouts/version_two/version_two'
   end
 
   def version_two_page_one
-    @css_file = 'version_two/version_two.css'
     render 'home/version_two/page_one', layout: 'layouts/version_two/version_two'
   end
 
   def version_two_page_two
-    @css_file = 'version_two/version_two.css'
     render 'home/version_two/page_two', layout: 'layouts/version_two/version_two'
   end
 
   def version_two_page_three
-    @css_file = 'version_two/version_two.css'
     render 'home/version_two/page_three', layout: 'layouts/version_two/version_two'
   end
+
+  private
+
+  def set_css_file
+    case action_name
+    when 'version_one', 'version_one_page_one', 'version_one_page_two', 'version_one_page_three'
+      @css_file = 'version_one/version_one.css'
+    when 'version_two', 'version_two_page_one', 'version_two_page_two', 'version_two_page_three'
+      @css_file = 'version_two/version_two.css'
+    end
+  end
+
 end
